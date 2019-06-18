5d48e0cd6a8ecd7e09689a5fa2fec137729edd8b
Salman Ahmad <salman.ahmad@linentech.net>
2019-06-18 22:18:56 +0500

    pull_md script changes

--- pull_md.sh
+++ pull_md.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git checkout gh-pages
+git remote add upstream https://github.com/salmanahmad456/md-files.git
 git pull upstream master --allow-unrelated-histories --no-edit
 git push origin gh-pages
