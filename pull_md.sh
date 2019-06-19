59c2d2d3f44f6b8b2100574276a9943b60f38ce7
RealizingBlockchain <zurichblockchainrnd@users.noreply.github.com>
2019-06-19 20:24:42 +0200

    Changed script

--- pull_md.sh
+++ pull_md.sh
@@ -8,6 +8,8 @@ git checkout gh-pages
 # set MD source repo (quick setup https mode link) - adds a repo to communicate with - needed to pull from other repositories the changes
 git remote add upstream https://github.com/Zurich-Blockchain-RnD/Just-the-md-files.git
 
+# pull changes from gh-pages
+git pull origin gh-pages --no-edit
 
 # pull changes from MD repo. --no-edit means it does not ask for description (no need to describe action of pull request)
 # --allow-unrelated-histories override error message of no link between repositories
@@ -19,3 +21,5 @@ git remote add upstream https://github.com/Zurich-Blockchain-RnD/Just-the-md-fil
 git pull upstream master --allow-unrelated-histories --no-edit
 # push changed files from MD repo to pages repo
 git push origin gh-pages
+# pause bash
+$SHELL
