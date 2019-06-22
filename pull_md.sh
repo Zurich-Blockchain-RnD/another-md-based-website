43f8481f06d2e8b5830c1b15a2714875d687ae7c
Salman Ahmad <salman.ahmad@linentech.net>
2019-06-22 12:16:09 +0500

    script change

--- pull_md.sh
+++ pull_md.sh
@@ -4,12 +4,12 @@
 # both first command are executed repeatedly in case branch was changed or upstream
 
 # set branch (for pages repo)
-git checkout gh-pages
+git checkout master
 # set MD source repo (quick setup https mode link) - adds a repo to communicate with - needed to pull from other repositories the changes
 git remote add upstream https://github.com/Zurich-Blockchain-RnD/Just-the-md-files.git
 
 # pull changes from gh-pages
-git pull origin gh-pages --no-edit
+git pull origin master --no-edit
 
 # pull changes from MD repo. --no-edit means it does not ask for description (no need to describe action of pull request)
 # --allow-unrelated-histories override error message of no link between repositories
@@ -20,7 +20,7 @@ git pull origin gh-pages --no-edit
 
 git pull upstream master --allow-unrelated-histories --no-edit
 # push changed files from MD repo to pages repo
-git push origin gh-pages
+git push origin master
 
 # change directory to _site for all HTML generated files 
 cd _site
